85e1e434a1558ac25b07a99d00daa5322ae7232d
Tetiana Bodnarchuk <165067421+TetianaBodnarchuk@users.noreply.github.com>
2024-07-23 02:11:06 +0300

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -4,20 +4,17 @@ def get_cats_info(path) -> list[dict]:
     try:
         with open(path/'cat.txt', 'r', encoding='utf-8') as file:
             list_cats = []
-
             for line in file:
                 content = line.strip().split(',')
 
                 if len(content) == 3:
-                    info = {'id': content[0], 'name': content[1], 'age': content[2]}
-                    list_cats.append(info)
-                    print(list_cats)
-                    print('*'*5)
+                    dict_cat = {'id': content[0], 'name': content[1], 'age': content[2]}
+                    list_cats.append(dict_cat)
                 else:
-                    print(f'Частина необхідних даних відсутня')
-
+                    print(f'У строці {line.strip()} не вистачає даних.')
+        return list_cats
     except FileNotFoundError:
         print('Файл не знайдено.')
-
+        
 cats_info = get_cats_info(Path(__file__).parent)
 print(cats_info)
