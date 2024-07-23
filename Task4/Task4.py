309f637b4dfd3d0d1f91715fb6ff548647cb524a
Tetiana Bodnarchuk <165067421+TetianaBodnarchuk@users.noreply.github.com>
2024-07-23 13:33:48 +0300

    Update Task4.py

--- Task4/Task4.py
+++ Task4/Task4.py
@@ -1,61 +1,52 @@
-
-def parse_input(user_input: str) -> tuple:
+def parse_input(user_input: str) -> str:
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
 
-def add_contact(args, contacts):
-    name, phone = args
-    contacts[name] = phone
-    return "Contact added."
-
-'''
-чи вірний регулярний вираз '\d+.\d+' для пошуку в списку данних які потім можна в флоат перетворити?
-=
-'\d+\.\d+' - так краще
-'\d+(\.\d+)?'- або так
-Не забуd' про r - r'\d+(\.\d+)?'
-(що значить r ? - Звичайний рядок може не так зрозуміти символи в середині регулярного виразу. А коли  пишешь r попереду ти позначаєшь рядок як raw, тобто то він має бути інтерпретуватись так як написано
-'''
-
-def change_contact(args, contacts):
-    name, phone = args
-    contacts[name] = phone
-    return "Contact updated."
-
-'''
-def add_contact(args, contacts):
+def add_contact(args: list, contacts: dict) -> str:
     try:
-        if len(args) != 2:
-            raise ValueError("Невірна кількість аргументів. Потрібно вказати ім'я та телефон.")
         name, phone = args
-        contacts[name] = phone
-        return "Контакт додано."
-    except ValueError as e:
-        return str(e)
-def main():
-    contacts = {}
-    while True:
-        command = input("Введіть команду (add, change, exit): ")
-        if command == 'exit':
-            break
-        args = input("Введіть ім'я та телефон: ").split()
-        if command == 'add':
-            print(add_contact(args, contacts))
-if __name__ == "__main__":
-    main()
-'''
+        if name.isalpha() and phone.isdigit():
+            contacts[name] = phone
+            return 'Contact added.'
+        else:
+            return f'{name} is not word or {phone} is not number.'
+    except ValueError:
+        return 'Invalid number of arguments. Require name and one phone number.'
 
-def show_phone(args, contacts):
-    name = args[0]
-    if name in contacts:
-        return contacts[name]
-    else:
-        return "This contact doesn't exist"
+def change_contact(args: list, contacts: dict) -> str:
+    try:
+        name, phone = args
+        if name.isalpha() and phone.isdigit():
+            if name in contacts:
+                contacts[name] = phone
+                return 'Contact updated.'
+            else:
+                return 'Name not found.'
+        else:
+            return f'{name} is not word or {phone} is not number.'            
+    except ValueError:
+        print('Invalid number of arguments. Require name and one phone number.')
 
-def show_all(args, contacts):
-    name, phone = args
-    return contacts[name]
+def show_phone(args: list, contacts: dict) -> str:
+    try:
+        name = args[0]
+        if name in contacts and len:
+            return contacts[name]
+        else:
+            return "This contact doesn't exist"
+    except Exception:
+        print('Invalid command.')
+
+def show_all(contacts: dict) -> str:
+    try:
+        if not contacts:
+            return f'The contacts list is empty.'
+        else:
+            for name, phone in contacts.items():
+                print(f'{name}: {phone}')
+    except Exception:
+        print('Invalid command.')
 
 def main():
     contacts = {}
@@ -63,7 +54,6 @@ def main():
     while True:
         user_input = input("Enter a command: ")
         command, *args = parse_input(user_input)
-
         if command in ["close", "exit"]:
             print("Good bye!")
             break
@@ -72,21 +62,13 @@ def main():
         elif command == "add":
             print(add_contact(args, contacts))
         elif command == "change":
-            print(add_contact(args, contacts))
+            print(change_contact(args, contacts))
         elif command == "phone":
-            print(add_contact(args, contacts))
+            print(show_phone(args, contacts))
         elif command == "all":
-            print(add_contact(args, contacts))
+            print(show_all(contacts))
         else:
             print("Invalid command.")
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-if __name__ == '__main__':
-    main()
-
